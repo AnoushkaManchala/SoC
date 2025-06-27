658ba72c6a8f281585afc455f459efee082ff2b7
AnoushkaManchala <manchala.anoushka@gmail.com>
2025-06-27 13:29:12 +0530

    Update basic_syntax.py

--- basic_syntax.py
+++ basic_syntax.py
@@ -5,3 +5,16 @@ c = "hello"
 d = True
 
 print(type(a), type(b), type(c), type(d))
+
+
+
+# Eroor handling
+try:
+    x = int(input("Enter a number: "))
+    print(100 / x)
+except ZeroDivisionError:
+    print("Can't divide by zero!")
+except ValueError:
+    print("Invalid input. Please enter a number.")
+finally:
+    print("Execution complete.")
